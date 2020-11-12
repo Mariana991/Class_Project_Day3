e422aa870bb6a4d49c002af6a67193bc6bada97b
Mariana991 <ma7643@yahoo.com>
2020-11-12 10:46:05 -0500

    learning process

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git Last day");
         // today is a beatiful day
+        // we are learning about Git and Inteliji
     }
 }
